15491721cb06977546f47c88b9d89803d8ae48ba
shes050117 <apple@Appleteki-MacBook-Pro.local>
2012-04-14 09:48:40 +0800

    Initial Commit

---
+++ test111/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test111
+//
+//  Created by Apple on 12/4/14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test111/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test111
+//
+//  Created by Apple on 12/4/14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
